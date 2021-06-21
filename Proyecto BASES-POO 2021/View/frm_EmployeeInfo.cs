d840d6a776ef877681612687aaeacdc1a9562700
Cristian Arturo Hernández Flores <00216920@uca.edu.sv>
2021-06-21 17:49:17 -0600

    Updating Employee Branch

---
+++ Proyecto BASES-POO 2021/View/frm_EmployeeInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Proyecto_BASES_POO_2021.ProjectContext;
+using Proyecto_BASES_POO_2021.viewModels;
+
+namespace Proyecto_BASES_POO_2021
+{
+    public partial class frm_EmployeeInfo : Form
+    {
+
+        private bool showFromDB = false;
+        public frm_EmployeeInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_EmployeeInfo_Load(object sender, EventArgs e)
+        {
+
+            if (!showFromDB)
+            {
+                using (var db = new PROJECT_BD_POOContext())
+                {
+                    var employee = db.Managers.ToList();
+                    var mappedDS = new List<ManagerVm>();
+                    
+                    employee.ForEach(e => mappedDS.Add(ProjectMapper.mapManagerVm(e)));
+
+                    dgv_employeeInfo.DataSource = mappedDS;
+                }
+            }
+
+            
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Proyecto BASES-POO 2021/ViewModels/ManagerVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_BASES_POO_2021.ProjectContext;
+
+namespace Proyecto_BASES_POO_2021.viewModels
+{
+    public class ManagerVm
+    {
+        public int IdManager { get; set; }
+        public int IdTypeEmployee { get; set; }
+        public string ManagerName { get; set; }
+        public string ManagerAddress { get; set; }
+        public string InstitutionalEmail { get; set; }
+        
+        
+        //public virtual TypeEmployee IdTypeEmployeeNavigation { get; set; }
+    }
+}
